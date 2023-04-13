914ada21237959012b936ebe7c4b2a12f7b883cf
Gabriel Miranda <gabrielmirandabarros98@gmail.com>
2023-04-13 09:13:45 -0300

    Nova Concessionaria

---
+++ src/main/java/com/primeirotrampodev/NovaConcessionaria/NovaConcessionariaApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.NovaConcessionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaConcessionariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaConcessionariaApplication.class, args);
+	}
+
+}
